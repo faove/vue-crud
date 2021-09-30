ec270113c3617aef9b450b0f1889bc41012f12b4
Francisco Alvarez <faove@hotmail.com>
2021-09-30 15:22:58 -0300

    Creando un proyecto Vue-CRUD

---
+++ src/components/HelloWord.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg2 }}</h1>
+        <h1>{{ texto }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HelloWord",
+    props: {
+        msg: String
+    },
+    data(){
+        return {
+            msg2: "otro",
+            texto: "hola desde el Hello World!!"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NewComponente.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h3>{{ valor }}</h3>
+        <div class="background: red">
+            <HelloWord></HelloWord>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import HelloWord from './HelloWord.vue';
+
+export default {
+    name: "newcomponente",
+    components: { HelloWord },
+    data(){
+        return{
+            valor: "valor del componente"
+        };
+    },
+}
+</script>
